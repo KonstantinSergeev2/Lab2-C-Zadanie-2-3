0533a8578bc050740830f632babeb72385c05c7b
KonstantinSergeev2 <kostyasergeev28821@gmail.com>
2024-11-08 03:27:34 +0500

    Add files via upload

---
+++ Лабораторная работа #2, ЯП C#, Задание 2-3, Сергеев Константин/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TimeOrig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите часы и минуты для первого времени: ");
+            byte hours1 = GetInput("Часы (от 0 до 23): ", 23);
+            byte minutes1 = GetInput("Минуты (от 0 до 59):", 59);
+            Time time1 = new Time(hours1, minutes1);
+
+            Console.WriteLine("Введите часы и минуты для второго времени: ");
+            byte hours2 = GetInput("Часы (от 0 до 23): ", 23);
+            byte minutes2 = GetInput("Минуты (от 0 до 59):", 59);
+            Time time2 = new Time(hours2, minutes2);
+
+            // выполнение вычитания времени (из задания 2)
+            Time result = time1.Subtract(time2);
+            Console.WriteLine($"Результат вычитания: {result}");
+
+            // унарная операция "--" (из задания 3)
+            Console.WriteLine("\nУнарное вычитание минуты из результата: ");
+            result--;
+            Console.WriteLine($"После уменьшения результата на минуту: {result}");
+
+            // Операции приведения типа (из задания 3)
+            byte hours = (byte)result; // явное приведение
+            Console.WriteLine($"Количество часов результата (без минут): {hours}");
+
+            bool isNonResultNull = result; // неявное приведение 
+            Console.WriteLine($"Является ли результат ненулевым? {isNonResultNull}");
+
+            // бинарная операция сложение двух объектов времени (из задания 3)
+            Console.WriteLine("\nВыполним бинарную операцию сложения двух времён: ");
+            Time sumResult = time1 + time2;
+            Console.WriteLine($"Результат сложения: {sumResult}");
+            
+            // бинарная операция добавления минут к результату (из задания 3)
+            Console.WriteLine("\nДобавляем минуты к результату: ");
+            byte minutesToAdd = GetInput("Сколько минут добавить? ", byte.MaxValue);
+            Time addeadMinutesResult = result + minutesToAdd;
+            Console.WriteLine($"Результат после добавления минут: {addeadMinutesResult}");
+        }
+        static byte GetInput(string message, byte maxValue)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (byte.TryParse(Console.ReadLine(), out byte value) && value <= maxValue)
+                {
+                    return value; // если ввод корректный, возвращаем значение
+                }
+                Console.WriteLine($"Пожалуйста, введите корректное значение от 0 до {maxValue}" );
+            }
+        }
+    }
+}
---
+++ Лабораторная работа #2, ЯП C#, Задание 2-3, Сергеев Константин/Time.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+
+namespace TimeOrig
+{
+    public class Time
+    {
+        private byte hours; // поле для хранения часов
+        private byte minutes; // поле для хранения минут
+
+        // свойство для управлением значением часов
+        public byte Hours
+        {
+            get { return hours; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentOutOfRangeException("Часы должны быть в диапозоне от 0 до 23. ");
+                }
+                hours = value;
+            }
+        }
+        // свойство для управления значением минут
+        public byte Minutes
+        {
+            get { return minutes; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentOutOfRangeException("Минуты должны быть в диапозоне от 0 до 59. ");
+                }
+                minutes = value;
+            }
+        }
+        // конструктор по умолчанию
+        public Time()
+        {
+            hours = 0;
+            minutes = 0;
+        }
+        // конструктор с параметрами для задания значений часов и минут
+        public Time(byte hours, byte minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+        public Time Subtract(Time other)
+        {
+            int TotalMinutes1 = hours * 60 + minutes;
+            int TotalMinutes2 = other.hours * 60 + other.minutes;
+            int resultMinutes = TotalMinutes1 - TotalMinutes2;
+
+            // Если результат отрицательный, то добавляем одни сутки (или 1440 минут)
+            if (resultMinutes < 0)
+            {
+                resultMinutes += 24 * 60;
+            }
+
+            byte resultHours = (byte)(resultMinutes / 60);
+            byte resultMinutesOnly = (byte)(resultMinutes % 60);
+
+            return new Time(resultHours, resultMinutesOnly);
+        }
+
+        // оператор явного приведения типа в byte - результат количество часов
+        public static explicit operator byte(Time time)
+        {
+            return time.hours;
+        }
+
+        // оператор неявного приведения типа в bool - результатом является true, если часы и минуты не равны нулю.
+        public static implicit operator bool(Time time)
+        {
+            return time.hours != 0 || time.minutes != 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{hours:D2}:{minutes:D2}";
+        }
+
+        public static Time operator --(Time time)
+        {
+            if (time.minutes == 0)
+            {
+                time.minutes = 59;
+                if (time.hours == 0)
+                {
+                    time.hours = 23;
+                }
+                else
+                {
+                    time.hours--;
+                }
+            }
+            else
+            {
+                time.minutes--;
+            }
+            return time;
+        }
+        public static Time operator +(Time time, byte minutesToAdd)
+        {
+            int totalMinutes = time.hours * 60 + time.minutes + minutesToAdd;
+            byte resultHours = (byte)(totalMinutes / 60 % 24);
+            byte resultMinutes = (byte)(totalMinutes % 60);
+            return new Time(resultHours, resultMinutes);
+        }
+
+        public static Time operator +(Time time1, Time time2)
+        {
+            int totalMinutes = time1.hours * 60 + time1.minutes + time2.hours * 60 + time2.minutes;
+            byte resultHours = (byte)(totalMinutes / 60 % 24);
+            byte resultMinutes = (byte)(totalMinutes % 60);
+            return new Time(resultHours, resultMinutes);
+        }
+    }
+}
